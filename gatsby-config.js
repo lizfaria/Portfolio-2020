1be4e52deb0d2c35b12adfeea550541c42eedca3
lizfaria <efaria93@gmail.com>
2019-12-16 19:36:59 -0500

    Added netlify cms fields

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
     author: `@gatsbyjs`,
   },
   plugins: [
-    `gatsby-plugin-netlify-cms`
+    `gatsby-plugin-netlify-cms`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,39 @@
 import React from "react"
-import { Link } from "gatsby"
-
+import { graphql, Link } from "gatsby"
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+export default function IndexPage({data 
+  // this prop will be injected by the GraphQL query below.
+}) {
+  return(
+    <Layout>
+      <SEO title="Home" />
+      <h1>Under Construction</h1>
+    
+      {/* <Link to="/page-2/">Go to page 2</Link> */}
+    </Layout>
+  )
+}
 
-export default IndexPage
+
+// export const pageQuery = graphql`
+//   query HomePageQuery {
+//     allMarkdownRemark(filter: {frontmatter: {templateKey: {eq: "home"}}}) {
+//       edges {
+//         node {
+//           frontmatter {
+//             title
+//             headshot {
+//               image
+//             }
+//             body
+//             resume {
+//               file
+//             }
+//           }
+//         }
+//       }
+//     }
+//   }
+// `;
