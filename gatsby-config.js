7fbcedd0a95e9d49c991995f9abdbab12816ad6e
lizfaria <efaria93@gmail.com>
2020-01-02 11:16:24 -0500

    adding google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -73,6 +73,14 @@ module.exports = {
         ],
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-155272053-1",
+        head: true,
+        respectDNT: false,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
